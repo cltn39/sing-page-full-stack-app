dd6bbe57e50dc3773c816166a53164fcef38f2f1
Chisoo Song <cltn39@gmail.com>
2021-01-08 15:53:39 -0800

    Fully working application

--- controller/controller.js
+++ controller/controller.js
@@ -1,28 +1,29 @@
 require("dotenv").config();
 const axios = require("axios");
 const db = require("../models");
-const key = process.env.API_KEY
+const key = process.env.API_KEY;
 
 module.exports = {
   picOfTheDay: function (req, res) {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=${key}`,)
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
       .then((response) => res.json(response.data))
       .catch((err) => res.status(422).json(err));
   },
   findAllPost: function (req, res) {
     db.Post.findAll({}).then(function (dbPost) {
-      res.json(dbPost);
+      res.json(dbPost).catch((err) => res.status(422).json(err));
     });
   },
   createNewPost: function (req, res) {
     db.Post.create({
       title: req.body.title,
       body: req.body.body,
-      create_at: req.body.created_at
+      create_at: req.body.created_at,
     })
-      .then(function(dbPost) {
-        res.json(dbPost)
-      });
+      .then(function (dbPost) {
+        res.json(dbPost);
+      })
+      .catch((err) => res.status(422).json(err));
   },
 };
--- routes/api/db.js
+++ routes/api/db.js
@@ -4,5 +4,5 @@ const controller = require("../../controller/controller");
 // Matches with "/api/posts"
 router.route("/").get(controller.findAllPost);
 // Matches with "/api/posts/new"
-router.route("/new").get(controller.createNewPost);
+router.route("/new").post(controller.createNewPost);
 module.exports = router;
